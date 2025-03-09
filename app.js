6d9544f5c49127b268b81e7745f88eb78d251a5a
Thewarmo <abayona123@hotmail.com>
2025-03-09 12:09:28 -0500

    cambio para el puerto de la aplicacion

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use("/citas", citaRoutes);
 app.use("/procedimientos", procedimientoRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
